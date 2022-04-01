d1f6b5991cd4c0ea85c3933b6c7bf8b937ad9927
Hendrik Sebastian Voß <hendrik@familievoss.org>
2022-04-01 23:55:04 +0200

    day one as far known

---
+++ src/app/components/technical/sesion/session.component.ts
@@ -0,0 +1,21 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-session',
+  templateUrl: './session.component.html',
+  styleUrls: ['./session.component.scss']
+})
+export class SessionComponent implements OnInit {
+
+  @Input() heading: string = "";
+  @Input() time: string = "";
+  @Input() speaker: string = "";
+
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
