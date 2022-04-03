d3a7f4ae57897fe5b9cc4e29fb1e36062d82af52
Hendrik Sebastian Voß <hendrik@familievoss.org>
2022-04-03 16:04:32 +0200

    Layout

--- karma.conf.js
+++ karma.conf.js
@@ -25,11 +25,11 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/vision-days-companion'),
+      dir: require('path').join(__dirname, './coverage/vision-days-2022'),
       subdir: '.',
       reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
+        {type: 'html'},
+        {type: 'text-summary'}
       ]
     },
     reporters: ['progress', 'kjhtml'],
