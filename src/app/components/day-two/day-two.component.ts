3cfdfcd884ed40f516c220d5e14023f0620618e7
Hendrik Sebastian Voß <hendrik@familievoss.org>
2022-04-04 21:06:53 +0200

    Spinner while update

--- src/app/components/day-two/day-two.component.ts
+++ src/app/components/day-two/day-two.component.ts
@@ -9,5 +9,5 @@ export class DayTwoComponent {
 
   constructor() {
   }
-  
+
 }
--- src/app/services/prompt-update.service.ts
+++ src/app/services/prompt-update.service.ts
@@ -1,14 +1,23 @@
 import {Injectable} from '@angular/core';
 import {SwUpdate, UpdateAvailableEvent} from "@angular/service-worker";
+import {NgxSpinnerService} from "ngx-spinner";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PromptUpdateService {
 
-  constructor(updates: SwUpdate) {
+  constructor(updates: SwUpdate, private spinnerService: NgxSpinnerService) {
     updates.available.subscribe((event: UpdateAvailableEvent) => {
-      updates.activateUpdate().then(() => document.location.reload());
+
+      updates.activateUpdate().then(async () => {
+        this.spinnerService.show();
+        setTimeout(async () => {
+          document.location.reload()
+          this.spinnerService.hide();
+        }, 600)
+
+      });
     });
   }
 }
