d2fcfc1070e96e166e0416bb607949e2a609b36f
Hendrik Sebastian Voß <hendrik@familievoss.org>
2022-04-08 19:51:04 +0200

    Agenda updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {DayOneComponent} from "./components/day-one/day-one.component";
-import {HomeComponent} from "./components/home/home.component";
+import {LocationComponent} from "./components/location/location.component";
 import {DayTwoComponent} from "./components/day-two/day-two.component";
 import {DayThreeComponent} from "./components/day-three/day-three.component";
-import {ImpressumComponent} from "./components/impressum/impressum.component";
+import {ImpressumComponent} from "./components/technical/impressum/impressum.component";
+import {FaqComponent} from './components/faq/faq.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
+  {path: '', component: LocationComponent},
+  {path: 'faq', component: FaqComponent},
   {path: 'day-one', component: DayOneComponent},
   {path: 'day-two', component: DayTwoComponent},
   {path: 'day-three', component: DayThreeComponent},
