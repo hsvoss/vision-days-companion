c212d8a27485b8e1f2ee66e1e378f82c1f29cffb
Hendrik Sebastian Voß <hendrik@familievoss.org>
2022-04-09 18:19:50 +0200

    Widescreen Navigation for under 360p

--- src/app/components/technical/navigation/navigation.component.ts
+++ src/app/components/technical/navigation/navigation.component.ts
@@ -1,14 +1,25 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {DrawerService} from "../../../services/drawer.service";
+import {BreakpointObserver, BreakpointState} from "@angular/cdk/layout";
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss']
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit {
+  oldSmartphone: boolean = false;
 
-  constructor(private drawerService: DrawerService) {
+  constructor(private drawerService: DrawerService, private breakpointObserver: BreakpointObserver,) {
+  }
+
+
+  ngOnInit() {
+    this.breakpointObserver
+      .observe(['(max-height: 450px)'])
+      .subscribe((state: BreakpointState) => {
+        this.oldSmartphone = state.matches;
+      });
   }
 
   onClick() {
