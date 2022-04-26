24a355c034acf1f69a9bca4fbd84df0575656534
Hendrik Sebastian Voß <hendrik@familievoss.org>
2022-04-26 07:47:41 +0200

    Automatic switch Placeholders vs. Drum Conversation

--- src/app/components/day-two/day-two.component.ts
+++ src/app/components/day-two/day-two.component.ts
@@ -1,24 +1,22 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-day-two',
   templateUrl: './day-two.component.html',
   styleUrls: ['./day-two.component.scss']
 })
-export class DayTwoComponent implements OnInit {
+export class DayTwoComponent {
 
-  drums: 'Placeholder' | 'TimeIsUp' = 'Placeholder'
 
   constructor() {
   }
 
 
-  ngOnInit(): void {
-    const timeIsUp = new Date() > new Date('2022-05-19T12:45:00.000Z');
-    if (timeIsUp) {
-      this.drums = "TimeIsUp"
+  calcPlaceholder(): 'Placeholder' | 'TimeIsUp' {
+    if (new Date() > new Date('2022-05-19T12:45:00.000Z')) {
+      return 'TimeIsUp'
     } else {
-      this.drums = "Placeholder"
+      return 'Placeholder'
     }
   }
 
